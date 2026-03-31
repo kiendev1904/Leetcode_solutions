8debdcc3d9767189adefe5efb031cac24c496093
Phạm Trung Kiên <phamtrungkien1904@gmail.com>
2026-03-31 09:35:34 +0700

    Time: 3 ms (31.33%), Space: 9.4 MB (50.96%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.c
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.c
@@ -1,8 +1,11 @@
+
+/* Ý tưởng là sẽ dùng two point để duyệt đầu và cuối, khi gặp được nguyên âm ở cả 2 đầu thì sẽ swap cho nhau.
+*/
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-int check(char s){// check xem co la nguyen am khong , dung binary search
-    char str[] = {'A','E','I','O','U','a','e','i','o','u'};
+int check(char s){
+    /*char str[] = {'A','E','I','O','U','a','e','i','o','u'};
     int left = 0;
     int right = 9;
     while(left <= right){
@@ -17,7 +20,32 @@ int check(char s){// check xem co la nguyen am khong , dung binary search
     
     }
     return 0;
-
+*/
+    switch(s){
+        case 'A':
+            return 1;
+            
+        case 'E':
+            return 1;
+        case 'I':
+            return 1;
+        case 'O':
+            return 1;
+        case 'U':
+            return 1;
+        case 'a':
+            return 1;
+        case 'e':
+            return 1;
+        case 'i':
+            return 1;
+        case 'o':
+            return 1;
+        case 'u':
+            return 1;
+        default:
+            return 0;
+    }
 }
 char* reverseVowels(char* s) {
     int i =0;
