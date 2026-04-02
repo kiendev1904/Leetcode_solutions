86f8db96a02c537d2f5ac6260d559d59297164cd
Phạm Trung Kiên <phamtrungkien1904@gmail.com>
2026-04-02 14:05:40 +0700

    Time: 0 ms (100%), Space: 31 MB (30.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.c
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -4,18 +4,15 @@
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     int*result = (int*)malloc(numsSize*sizeof(int));
     if(result ==  NULL) exit(0);
-    int prefix = 1;
     int suffix = 1;
     result[0] = 1;
     for(int i = 1 ;i < numsSize ;i++){
-        prefix = prefix * nums[i - 1];
-        result[i] = prefix;
-
+        result[i] = result[i - 1] * nums[i -1];   
     }
     
-    for(int i = numsSize - 2; i >= 0 ; i--){
-        suffix = suffix*nums[i + 1];
+    for(int i = numsSize - 1; i >= 0 ; i--){
         result[i] = result[i] * suffix;
+        suffix = suffix * nums[i];
 
     }
     *returnSize = numsSize;
