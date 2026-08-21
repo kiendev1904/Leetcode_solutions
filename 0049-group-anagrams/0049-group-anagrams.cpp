6bf8fb8ce1a4b768205d99711db2b0492f206be2
Phạm Trung Kiên <phamtrungkien1904@gmail.com>
2026-08-21 11:27:10 +0700

    Time: 16 ms (59.16%), Space: 25 MB (81.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,48 @@
-class Solution{
+// Giải thuật sử dụng unordered map để truy cập được cái xâu qua key - value.
+// Sơ qua về thuật toán là việc nó tạo ra 1 cái key vì với mỗi xâu là hoán vị của nhau
+// thì từng chữ cái trong xâu sẽ giống hệt nhau nên nhiều xâu sẽ có cùng 1 key
+// Việc đơn giản là tạo 1 mảng với kích thước 26 vì đây là số chữ cái có trong bảng.
+// Sau đó duyệt mỗi xâu thì sẽ có được cái key đó, thêm key vơi value đó vào là xog.
+// Giải thuật 1: dùng việc xuât hiẹn của chữ cái để làm key.
+/*class Solution{
 public: 
     vector<vector<string>> groupAnagrams(vector<string>& strs){
       unordered_map<string, vector<string>> group;
-      for(auto &s : strs){
-        int count[26] = {0};
+      for(auto &s : strs){ 
+        int count[26] = {0}; // Khai báo mảng 36 phần tử.
         for(char c : s){
-          count[c - 'a']++;
+          count[c - 'a']++;// tạo 11 arr xâu bằng việc duyệt kí tự.
         }
-        string key = "";
+        string key = "";// tạo key để thêm vào map.
         key.reserve(26 * 4);
         for(int i = 0 ; i < 26; ++i){
           key += to_string(count[i]) + '#';
         }
+        group[key].push_back(s);
+
+
+      } 
+      vector<vector<string>> results;
+      for(auto &p : group){
+        results.push_back(p.second);
+      }
+      return results;
+
+
+
+    }
+
+};*/
+
+// Giải thuật 2: tôi sẽ dùng chính thứ tự từ điển của từng xâu để làm key.
+class Solution{
+public: 
+    vector<vector<string>> groupAnagrams(vector<string>& strs){
+      unordered_map<string, vector<string>> group;
+      for(auto &s : strs){ 
+        string key = s;
+        sort(key.begin(), key.end());
+        
         group[key].push_back(s);
 
 
